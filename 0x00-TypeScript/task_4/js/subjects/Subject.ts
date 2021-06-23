bc056d631f05d2947a70435f86a3e49305c6d94e
Daniel <danielct-97@hotmail.com>
2021-06-22 22:37:05 -0500

    9. Namespace & Declaration merging

---
+++ 0x00-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+/// <reference path="./Teacher.ts" />
+namespace Subjects {
+  export class Subject {
+    teacher: TeacherInterface
+
+    set setTeacher(teacher: TeacherInterface) {
+      this.teacher = teacher
+    }
+  }
+}
